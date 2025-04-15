55aade7cbc34e09971c18f9e41266ceb22d58bcb
karthik-4424 <karthik04424@gmail.com>
2025-04-15 19:21:55 +0530

    modified

--- src/routes/main.py
+++ src/routes/main.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from src.controllers import home_controller
+from src.controllers.home_controller import add_user,get_all_users,delete_user
 
 main = Blueprint('main', __name__)
 
@@ -11,14 +11,14 @@ def add_user():
     name = user_data.get('name')
 
     if name:
-        user = home_controller.add_user(name)
+        user = add_user(name)
         return jsonify({"id": user.id, "name": user.name}), 201  
     return jsonify({"error": "Name is required"}), 400
 
 #Get User route
 @main.route('/getUsers', methods=['GET'])
 def get_users():
-    users = home_controller.get_all_users()
+    users = get_all_users()
     
     if users:
         users_data = [{"id": user.id, "name": user.name} for user in users]
@@ -29,7 +29,7 @@ def get_users():
 #Delete User route
 @main.route('/deleteUser/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    success = home_controller.delete_user(user_id)
+    success = delete_user(user_id)
     if success:
         return jsonify({"message": "User deleted"}), 200
     return jsonify({"error": "User not found"}), 404
